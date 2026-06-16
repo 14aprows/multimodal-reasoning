fd4df719d87fe9686cba93a760ccf1178953cd9e
lwkys <Hasibuanfirman1404@gmail.com>
2026-06-16 20:06:01 +0700

    add DAQUAR dataloder config constants

--- src/configs/config.py
+++ src/configs/config.py
@@ -7,6 +7,7 @@ IMAGE_DIR = DATA_DIR / "images"
 DATA_CSV = DATA_DIR / "data.csv"
 TRAIN_CSV = DATA_DIR / "data_train.csv"
 EVAL_CSV = DATA_DIR / "data_eval.csv"
+TEST_CSV = EVAL_CSV
 
 TRAIN_IMAGES_LIST = DATA_DIR / "train_images_list.txt"
 TEST_IMAGES_LIST = DATA_DIR / "test_images_list.txt"
@@ -17,3 +18,8 @@ ANSWER_SPACE = DATA_DIR / "answer_space.txt"
 PAD_TOKEN = "<PAD>"
 UNK_TOKEN = "<UNK>"
 MAX_QUESTION_LENGTH = 20
+
+IMAGE_SIZE = 224
+BATCH_SIZE = 32
+NUM_WORKERS = 0
+TOP_K_ANSWERS = None
